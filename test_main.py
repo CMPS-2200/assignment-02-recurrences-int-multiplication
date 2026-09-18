4326a055b3a99b15879d71cded8cc5ee36794b75
Aaron Maus <amaus@tulane.edu>
2026-09-18 15:10:27 -0500

    Add additional tests

--- test_main.py
+++ test_main.py
@@ -7,7 +7,11 @@ from main import *
 # 5 pts
 def test_quadratic_multiply():
     assert quadratic_multiply(BinaryNumber(2), BinaryNumber(2)) == 2*2
+    assert quadratic_multiply(BinaryNumber(3), BinaryNumber(4)) == 3*4
+    assert quadratic_multiply(BinaryNumber(9), BinaryNumber(13)) == 9*13
 
 # 5 pts
 def test_subquadratic_multiply():
     assert subquadratic_multiply(BinaryNumber(2), BinaryNumber(2)) == 2*2
+    assert subquadratic_multiply(BinaryNumber(3), BinaryNumber(4)) == 3*4
+    assert subquadratic_multiply(BinaryNumber(9), BinaryNumber(13)) == 9*13
